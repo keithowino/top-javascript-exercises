16fd3f2e9fd9892303eb364013c06a3e1efb143f
Keith Owino <designsolutions1629@gmail.com>
2023-09-13 13:42:22 +0300

    Add palindromes excersice solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let output = '';
+    let target =  str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let targetMirror = target.split('').reverse().join('');
+    if(target === targetMirror){
+        output = true;
+    }else{
+        output = false;
+    };
+    return output;
 };
+palindromes('Ddd');
 
 // Do not edit below this line
 module.exports = palindromes;
