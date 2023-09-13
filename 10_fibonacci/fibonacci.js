361792690a90cb9fcef68850e5b50bdafe481ff1
Keith Owino <designsolutions1629@gmail.com>
2023-09-13 18:05:35 +0300

    Add fibonacci excersice solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
 
+    let entryOne = 1;
+    let entryTwo = 0;
+    let sum;
+
+    for(let i = 2; i <= num; i++){
+
+        sum = entryOne + entryTwo;
+        entryTwo = entryOne;
+        entryOne = sum;
+
+    };
+
+    return entryOne;
 };
+
+fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
