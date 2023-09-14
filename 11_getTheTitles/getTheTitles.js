f86e9e288db7ab9f018aa4a2f6a8db6dc14b492b
Keith Owino <designsolutions1629@gmail.com>
2023-09-14 12:31:08 +0300

    Add getTheTitle exsercise solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    }, 
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+];
+const getTheTitles = function(item) {
+    const name = [];
+    let len = item.length - 1;
+    for(let i = 0; i <= len; i++){
+        name.push(item[i].title);
+    };
+    return name;
 };
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
